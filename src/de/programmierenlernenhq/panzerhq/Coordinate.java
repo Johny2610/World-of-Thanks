2bd06ebb8626f23cf6f248b2285fff557c72c231
Johannes Weiß <johajuliweis@outlook.de>
2016-06-01 16:08:56 +0200

    Changing project

---
+++ src/de/programmierenlernenhq/panzerhq/Coordinate.java
@@ -0,0 +1,32 @@
+package de.programmierenlernenhq.panzerhq;
+
+/**
+ *
+ * @author ProgrammierenLernenHQ
+ */
+public class Coordinate {
+    
+    private double x;
+    private double y;
+    
+    public Coordinate(double x, double y) {
+        
+    }             
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        
+    }
+
+}
